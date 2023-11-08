fd107f0b20962f2e38c1042b2651b6de3df177bf
darq <prototypid@gmail.com>
2023-11-08 22:50:54 +0530

    Add book model

---
+++ models/book.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  summary: {
+    type: String,
+    required: String,
+  },
+});
+
+export default mongoose.model("Book", bookSchema);
