25e048da7192fc6e32f813fd6f748ebaca4fa75b
darq <prototypid@gmail.com>
2023-11-08 23:42:39 +0530

    Add routes to add and update book

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -22,8 +22,45 @@ export const getBookById = async (req, res, next) => {
   return res.status(200).json({ status: "Success", book: book });
 };
 
+export const updateBook = async (req, res, next) => {
+  try {
+    const { title, summary } = req.body;
+
+    if (!title || !summary) return next(createError(400, "Invalid data"));
+
+    const book = await Book.findById(req.params.id);
+
+    if (!book) return next(createError(404, "Book not found!"));
+
+    book.title = title;
+    book.summary = summary;
+
+    const updatedBook = await book.save();
+    return res.status(200).json({ staus: "Success", book: updatedBook });
   } catch (err) {
-    next(err);
+    return next(err);
+  }
+};
+
+export const newBook = async (req, res, next) => {
+  try {
+    const { title, summary } = req.body;
+
+    if (!title || !summary) {
+      return next(createError(400, "Send the appropriate data"));
+    }
+
+    const book = new Book({ ...req.body, author: "admin" });
+
+    await book.save();
+
+    if (!book) {
+      return next(createError());
+    }
+
+    return res.status(201).json({ status: "Success", book: book });
+  } catch (err) {
+    return next(err);
   }
 };
 
