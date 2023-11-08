ba9e65c5ec80a98da82de36dc1deccafbedfda9d
darq <prototypid@gmail.com>
2023-11-08 23:42:02 +0530

    Update get books route and add get book route

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -6,7 +6,22 @@ export const getAllBooks = async (req, res, next) => {
     const books = await Book.find();
     console.log(books);
 
-    return res.json({ stauts: "Success", books: books });
+    return res.json({ status: "Success", books: books });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const getBookById = async (req, res, next) => {
+  const book = await Book.findById(req.params.id);
+
+  if (!book) {
+    return next(createError(404, "Book not found!"));
+  }
+
+  return res.status(200).json({ status: "Success", book: book });
+};
+
   } catch (err) {
     next(err);
   }
